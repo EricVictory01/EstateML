ff24762394c087ecd71928ec25e9d257bdad7b54
STUN-FI HUB <ejinnafavour@gmail.com>
2026-04-29 19:22:47 +0100

    Refactor load_and_prep_data to remove clustering
    Removed K-Means clustering from data preparation function.

--- utils.py
+++ utils.py
@@ -24,8 +24,4 @@ def load_and_prep_data(filepath="nigeria_houses_data.csv"):
     scaler = MinMaxScaler()
     df_scaled = scaler.fit_transform(df[features])
     
-    # Initialize and fit K-Means (K=5 based on the elbow method results)
-    kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
-    df['cluster'] = kmeans.fit_predict(df_scaled)
-    
-    return df, kmeans, scaler, le_town, le_state
+    return df, scaler, le_town, le_state
